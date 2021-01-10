6e97557739b707ba09e96cc76152a57d47b688d1
qinkaiyuan <qinkaiyuan94@163.com>
2021-01-10 16:05:17 +0800

    删除 内容的不违规状态，不在使用统一状态，而是维护各个状态

--- src/main/java/com/qingchi/server/common/TestMain.java
+++ src/main/java/com/qingchi/server/common/TestMain.java
@@ -3,22 +3,28 @@ package com.qingchi.server.common;
 import com.huaban.analysis.jieba.JiebaSegmenter;
 import com.huaban.analysis.jieba.SegToken;
 import com.qingchi.base.utils.KeywordsUtils;
+import lombok.experimental.var;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestMain {
     public static void main(String[] args) {
-        JiebaSegmenter segmenter = new JiebaSegmenter();
-        String[] sentences =
-                new String[]{"这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱Python和C++。", "我不喜欢日本和服。", "雷猴回归人间。",
-                        "工信处女干事每月经过下属科室都要亲口交代24口交换机等技术性器件的安装工作", "结果婚的和尚未结过婚的"};
-        for (String sentence : sentences) {
-            List<SegToken> fsd = segmenter.process(sentence, JiebaSegmenter.SegMode.INDEX);
-            for (SegToken segToken : fsd) {
-                System.out.println(segToken.word);
-            }
-        }
+        String str = "4654asfsdrq555erwesdaf456";
+        String numAryReg = "/\\d+/g";
+        List<String> numAry = getMatchers(numAryReg, str);
+        System.out.println(numAry.size());
     }
 
+    static List<String> getMatchers(String regex, String source) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(source);
+        List<String> list = new ArrayList<>();
+        while (matcher.find()) {
+            list.add(matcher.group());
+        }
+        return list;
+    }
 
 }
