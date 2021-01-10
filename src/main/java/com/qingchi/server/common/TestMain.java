8db10116772b324e3f6abeaa2fd67f90bc8aaf0a
qinkaiyuan <qinkaiyuan94@163.com>
2021-01-11 00:19:36 +0800

    删除 内容的不违规状态，不在使用统一状态，而是维护各个状态

--- src/main/java/com/qingchi/server/common/TestMain.java
+++ src/main/java/com/qingchi/server/common/TestMain.java
@@ -11,10 +11,20 @@ import java.util.regex.Pattern;
 
 public class TestMain {
     public static void main(String[] args) {
-        String str = "4654asfsdrq555erwesdaf456";
-        String numAryReg = "/\\d+/g";
+        String hasNot5Reg = ".*[^5]+.*";
+
+        Pattern pattern = Pattern.compile(hasNot5Reg);
+        //是否包含非5
+        String text1 = "15";
+        Matcher matcher = pattern.matcher(text1);
+
+        //是否包含非5
+        System.out.println(!matcher.matches());
+        //全是5， 存在拒5，别5，这样的可能
+        /*String str = "4654asfsdrq555erwesdaf456";
+        String numAryReg = "\\d+";
         List<String> numAry = getMatchers(numAryReg, str);
-        System.out.println(numAry.size());
+        System.out.println(numAry.size());*/
     }
 
     static List<String> getMatchers(String regex, String source) {
